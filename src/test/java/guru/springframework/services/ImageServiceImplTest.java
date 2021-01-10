08fbc9638e092af8e5ebb149d407d1b51a0472b9
Mateusz Wołek <volkosky@gmail.com>
2021-01-10 21:29:01 +0100

    images handled

---
+++ src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -0,0 +1,58 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+public class ImageServiceImplTest {
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        // given
+        Long id = 1L;
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "imagefile", "testing.txt", "text/plain",
+                "Volkosky guru".getBytes()
+        );
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> captor = ArgumentCaptor.forClass(Recipe.class);
+
+        // when
+        imageService.saveImageFile(id, multipartFile);
+
+        // then
+        verify(recipeRepository, times(1)).save(captor.capture());
+        Recipe savedRecipe = captor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
+
+}
